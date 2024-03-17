5c1b9a497033ecfc4ca52b2a2da2729589517c93
Shashikant Maurya <114757626+ssseworld@users.noreply.github.com>
2024-03-17 10:09:09 +0530

    Solve clerk error

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
-import { auth } from "@clerk/nextjs";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
-  const { userId } = auth();
   return (
-    <p className="text-3xl font-medium text-sky-700">Hello Eorld {userId}</p>
+    <p className="text-3xl font-medium text-sky-700">Hello Eorld 
+    <UserButton afterSignOutUrl="/" />
+    </p>
   );
 }
